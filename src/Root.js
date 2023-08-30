d8f063e01269c264448136978baba0e08dfb3b2a
Sourav Das <iamsouravdaswade420@gmail.com>
2023-08-30 08:44:37 +0530

    initial setup commit

---
+++ src/Root.js
@@ -0,0 +1,14 @@
+import {React,Component} from "react";
+import  './Root.css'
+export  default  class Root extends Component{
+    constructor() {
+        super();
+    }
+    render() {
+        return(
+            <div className={"container"}>
+                hi man
+            </div>
+        )
+    }
+}
